ffa5e3f00d9cb2a5f8653842574c962455a2d1a8
plapicola <p.lapicola@gmail.com>
2018-11-30 20:04:19 -0700

    Refactor node to seperately store title and rating. Test file updated to reflect

--- lib/node.rb
+++ lib/node.rb
@@ -1,13 +1,25 @@
 class Node
 
-  attr_reader :value
-
-  attr_accessor :left,
+  attr_accessor :title,
+                :rating,
+                :left,
                 :right
 
-  def initialize(value)
-    @value = value
+  def initialize(rating, title)
+    @rating = rating
+    @title = title
     @left = nil
     @right = nil
+  end
+
+  def children
+    children = 1
+    if @left != nil
+      children += @left.children
+    end
+    if @right != nil
+      children += @right.children
+    end
+    return children
   end
 end
--- test/node_test.rb
+++ test/node_test.rb
@@ -3,31 +3,42 @@ require_relative 'test_helper'
 class NodeTest < Minitest::Test
 
   def test_it_exists
-    node = Node.new(10)
+    node = Node.new(10, "Test")
 
     assert_instance_of Node, node
   end
 
-  def test_it_holds_a_value
-    node = Node.new(10)
+  def test_it_holds_a_rating_and_title
+    node = Node.new(10, "Test")
 
-    assert_equal 10, node.value
+    assert_equal 10, node.rating
+    assert_equal "Test", node.title
   end
 
   def test_it_has_a_left_reference
-    node = Node.new(10)
-    left_node = Node.new(8)
+    node = Node.new(10, "Test")
+    left_node = Node.new(8, "Left")
+    node.left = left_node
 
     node.left = left_node
     assert_equal left_node, node.left
   end
 
   def test_it_has_a_right_reference
-    node = Node.new(10)
-    right_node = Node.new(13)
+    node = Node.new(10, "Test")
+    right_node = Node.new(13, "Right")
+    node.right = right_node
 
     node.right = right_node
     assert_equal right_node, node.right
+  end
+
+  def test_it_tracks_children
+    node = Node.new(10, "Test")
+    left_node = Node.new(8, "Left")
+    node.left = left_node
+
+    assert_equal 2, node.children
   end
 
 end
