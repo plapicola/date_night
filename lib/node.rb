3cdb2c19514cc430e3b85a4cd2b4b4d9eaaf2e58
plapicola <p.lapicola@gmail.com>
2018-11-30 18:16:44 -0700

    Adds node

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,13 @@
 class Node
 
-  def initialize
+  attr_reader :value
+
+  attr_accessor :left,
+                :right
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
   end
 end
